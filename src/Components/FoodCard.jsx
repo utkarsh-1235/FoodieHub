ee886c85054ff76f2a45e7a927ac34b5ae96f3b3
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-02-21 18:01:39 +0530

    improvements in FoodCard.jsx adding Add to cart button and layout

--- src/Components/FoodCard.jsx
+++ src/Components/FoodCard.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function FoodCard({food}) {
   return (
-    <div className='flex flex-col w-80 h-[40%] bg-white shadow-lg rounded-lg overflow-hidden relative p-4 min-h-[450px]'>
+    <div className='flex flex-col w-80 h-[40%] bg-white shadow-lg rounded-lg overflow-hidden relative p-4 min-h-[350px]'>
        <div>{
         food.id && (<span 
-                     className='absolute top-2 left-2 bg-gray-800 text-xs px-2 py-1 rounded-t-lg'>
+                     className='absolute left-2 bg-gray-800 text-xs px-2 py-1 rounded-t-lg'>
               {food.id}
         </span>
        )}
@@ -18,14 +18,19 @@ function FoodCard({food}) {
        </div>
 
        <div className='p-4 flex flex-col flex-grow'>
-        <h3 className='text-lg font-semibold text-black'>{food.name}</h3>
-        <p className='text-black text-sm mt-2 flex-grow'>{food.desc}</p>
-       </div>
-       <div className="flex items-center justify-between mt-2 px-4 pb-4">
-          <span className="text-gray-700 font-medium">₹{food.price} for one</span>
-          <span className="bg-red-600 text-white text-xs px-2 py-1 rounded">
+        <div className='flex justify-between'>
+        <h3 className='text-sm font-semibold text-black'>{food.name}</h3>
+        <span className="bg-red-600 text-white text-xs px-2 py-1 rounded">
             {food.rating} ★
           </span>
+        </div>
+    
+        <p className='text-black text-sm mt-2 flex-grow'>{food.desc.slice(0,50)}............</p>
+       </div>
+       <div className="flex items-center justify-between gap-2 mt-2 px-4 pb-4">
+          <span className="text-gray-700 font-medium">₹{food.price} for one</span>
+          <button className='bg-red-500 p-2 rounded-lg font-bold hover:bg-red-600 text-sm'> Add to Cart</button>
+          
         </div>
     </div>
   )
