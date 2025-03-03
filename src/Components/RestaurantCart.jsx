35c25ad3357f0e82e361442718449c1f8a675684
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-03-03 21:18:32 +0530

    improvements in restaurantList.jsx and RestaurantCart.js

--- src/Components/RestaurantCart.jsx
+++ src/Components/RestaurantCart.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function RestaurantCart({id, name, img, rating, cuisine, delivery}) {
   return (
     <>
-    <div className='flex flex-col w-80 h-[40%] bg-white shadow-lg rounded-lg overflow-hidden relative p-4 min-h-[350px]'>
+    <div className='flex flex-col w-80 h-[40%] bg-white shadow-lg rounded-lg overflow-hidden relative p-4 min-h-[300px]'>
     <div className='relative w-full mt-3 ml-1 text-center'>
     <h3 className='text-2xl font-semibold text-black mb-3'>{name}</h3>
          <img 
@@ -18,6 +18,9 @@ function RestaurantCart({id, name, img, rating, cuisine, delivery}) {
         <span className="bg-red-600 text-white text-xs px-2 py-1 rounded">
             {rating} ★
           </span>
+
+          <p className='bg-red-600 text-white text-xs px-2 py-1 rounded'>{cuisine}</p>
+          <p className='bg-red-600 text-white text-xs px-2 py-1 rounded'>{delivery}min</p>
         </div>
         </div>
             </div>
--- src/Components/RestaurantList.jsx
+++ src/Components/RestaurantList.jsx
@@ -1,27 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Restaurants from '../Data/RestaurantsData';
 import RestaurantCart from './RestaurantCart';
 
 
 function RestaurantList() {
+    const [sort, setSort] = useState('rating');
+    const [search, setSearch] = useState("")
+
+    const filterRestaurant = Restaurants.filter((ind)=> ind.name.toLowerCase().includes(search.toLowerCase()))
+                                        .sort((a,b)=> sort === 'rating' ? b.rating - a.rating : a.deliveryTime - b.deliveryTime)
 
   return (
     <>
     <div className='bg-gradient-to-br from-red-200 to-red-800 text-white p-6 mx-auto w-full min-h-screen'>
         <div></div>
-        <div>
+        <div className='text-center'> 
         <h1 className='text-2xl mb-4 font-bold'> Restaurants Near You</h1>
 
          <input className='bg-white border rounded-md text-gray-800 py-1 px-2 text-center mb-4'
-                 placeholder='Search restaurants'/>
+                 placeholder='Search restaurants'
+                 value={search}
+                 onChange={(e)=>setSearch(e.target.value)}/>
 
-                 <div className='flex mb-4 gap-4'>
-                    <button className=' px-4 py-2 bg-red-400  rounded-md'>Best Rated</button>
-                    <button className='px-4 py-2 bg-red-400  rounded-md '>Fastest Delivery</button>
+                 <div className='flex mb-4 gap-4 justify-center'>
+                    <button className=' px-4 py-2 bg-red-400  rounded-md ' onClick={()=>setSort('rating')}>Best Rated</button>
+                    <button className='px-4 py-2 bg-red-400  rounded-md ' onClick={()=>setSort('delivery')}>Fastest Delivery</button>
                  </div>
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 md:gap-15 gap-10 p-6 items-start'>
          {
-            Restaurants.map((restaurant)=>(
+            filterRestaurant.map((restaurant)=>(
             <RestaurantCart key={restaurant.id} id = {restaurant.id} name = {restaurant.name} img = {restaurant.img} rating = {restaurant.rating} cuisine = {restaurant.cuisine} delivery = {restaurant.deliveryTime}/>
             ))
          }
